3e2265f75baca1d694fb2344737db1b74509e7f9
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-12-11 18:55:11 +0100

    create initial styled buttons

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -10,21 +10,24 @@ const StyledButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  border: 2px solid purple;
+  border: 1px solid black;
+  font-family: "AGRegular";
+  font-size: 20px;
   border-radius: 50%;
+  box-shadow: 0px 3px 8px #111, inset 0px 2px 3px #777;
   ${(props) => {
     switch (props.type) {
       case "black":
         return `background-color: black;
                 color: white;`;
       case "brown":
-        return `background-color: brown;
+        return `background-color: rgb(88, 65, 15);
                 color: white;`;
       case "green":
-        return `background-color: green;
+        return `background-color: rgb(1, 75, 32);
                 color: white`;
       case "yellow":
-        return `background-color: yellow;
+        return `background-color: rgb(253, 219, 28);
                 color: black;`;
       default:
         return `background-color: pink;`;
--- src/components/buttons/ButtonsLayout.jsx
+++ src/components/buttons/ButtonsLayout.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDivide,
+  faEquals,
+  faMinus,
+  faPercent,
+  faPlus,
+  faSquareRootAlt,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
+
 import Button from "./Button";
 
 const StyledButtonsLayout = styled.div`
@@ -13,35 +24,73 @@ const ButtonsLayout = () => {
   return (
     <StyledButtonsLayout>
       <Button type="green">M+</Button>
-      <Button type="green">M-</Button>
+      <Button type="green">M –</Button>
       <Button type="green">MR</Button>
-      <Button type="green" id="mc">MC</Button>
+      <Button type="green" id="mc">
+        MC
+      </Button>
       <Button type="green" passDownId="negative">
         +/-
       </Button>
       <Button type="brown" passDownId="sqrt">
-        Sq
+        <FontAwesomeIcon icon={faSquareRootAlt} />
       </Button>
-      <Button type="black" passDownId="seven">7</Button>
-      <Button type="black" passDownId="eight">8</Button>
-      <Button type="black" passDownId="nine">9</Button>
-      <Button type="brown" passDownId="divide">/</Button>
-      <Button type="brown" passDownId="percent">%</Button>
-      <Button type="black" passDownId="four">4</Button>
-      <Button type="black" passDownId="five">5</Button>
-      <Button type="black" passDownId="six">6</Button>
-      <Button type="brown" passDownId="multiply">x</Button>
-      <Button type="brown" passDownId="clear">CE</Button>
-      <Button type="black" passDownId="one">1</Button>
-      <Button type="black" passDownId="two">2</Button>
-      <Button type="black" passDownId="three">3</Button>
-      <Button type="brown" passDownId="subtract">-</Button>
-      <Button type="brown" passDownId="clear-current">C</Button>
-      <Button type="black" passDownId="zero">0</Button>
+      <Button type="black" passDownId="seven">
+        7
+      </Button>
+      <Button type="black" passDownId="eight">
+        8
+      </Button>
+      <Button type="black" passDownId="nine">
+        9
+      </Button>
+      <Button type="brown" passDownId="divide">
+        <FontAwesomeIcon icon={faDivide} />
+      </Button>
+      <Button type="brown" passDownId="percent">
+        <FontAwesomeIcon icon={faPercent} />
+      </Button>
+      <Button type="black" passDownId="four">
+        4
+      </Button>
+      <Button type="black" passDownId="five">
+        5
+      </Button>
+      <Button type="black" passDownId="six">
+        6
+      </Button>
+      <Button type="brown" passDownId="multiply">
+        <FontAwesomeIcon icon={faTimes} />
+      </Button>
+      <Button type="brown" passDownId="clear">
+        CE
+      </Button>
+      <Button type="black" passDownId="one">
+        1
+      </Button>
+      <Button type="black" passDownId="two">
+        2
+      </Button>
+      <Button type="black" passDownId="three">
+        3
+      </Button>
+      <Button type="brown" passDownId="subtract">
+        <FontAwesomeIcon icon={faMinus} />
+      </Button>
+      <Button type="brown" passDownId="clear-current">
+        C
+      </Button>
+      <Button type="black" passDownId="zero">
+        0
+      </Button>
       {/* TODO put a circular div in here instead */}
-      <Button type="brown">.</Button> 
-      <Button type="yellow" passDownId="equals">=</Button>
-      <Button type="brown" passDownId="add">+</Button>
+      <Button type="brown">.</Button>
+      <Button type="yellow" passDownId="equals">
+        <FontAwesomeIcon icon={faEquals} />
+      </Button>
+      <Button type="brown" passDownId="add">
+        <FontAwesomeIcon icon={faPlus} />
+      </Button>
     </StyledButtonsLayout>
   );
 };
