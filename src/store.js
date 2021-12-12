19b0237556f9ed25b15d66bd514e7a2fbb5d88ba
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-12-12 14:56:28 +0100

    powerOff only changes display state after transition is complete

--- src/store.js
+++ src/store.js
@@ -2,9 +2,15 @@ import create from "zustand";
 import { devtools } from "zustand/middleware";
 
 let useStore = (set) => ({
-  power: false,
+  power: true,
   powerOn: () => set((state) => ({ power: true })),
-  powerOff: () => set((state) => ({ power: false })),
+  powerOff: () => {
+    set((state) => ({ power: false }));
+    setTimeout(
+      () => set((state) => ({ display: "0" })),
+      200
+    );
+  },
 
   display: "1234.5678",
   setDisplay: (display) => set((state) => ({ display: display })),
