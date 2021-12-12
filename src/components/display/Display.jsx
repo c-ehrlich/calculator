82847bf45f2919fc4f0aaceb8b53a3f26e010718
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-12-12 14:57:04 +0100

    display styling

--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -6,16 +6,23 @@ import DisplayBorder from "./DisplayBorder";
 const DisplayText = styled.span`
   margin: 0;
   padding: 0;
-  color: rgb(63, 63, 63);
+  color: rgb(106, 117, 93);
   font-family: "D7Mono";
-  font-size: 48px;
+  font-size: 36px;
+  height: 32px;
+
+  &.power {
+    color: rgba(0,0,0,0.55)
+  }
 `;
 
 const Display = () => {
+  const power = useStore((state) => state.power);
   const display = useStore((state) => state.display);
+
   return (
     <DisplayBorder>
-      <DisplayText id="display">{display}</DisplayText>
+      <DisplayText id="display" className={power && "power"}>{display}</DisplayText>
     </DisplayBorder>
   );
 };
--- src/components/display/DisplayBorder.jsx
+++ src/components/display/DisplayBorder.jsx
@@ -1,28 +1,61 @@
 import React from "react";
 import styled from "styled-components";
+import useStore from "../../store";
+
+const StyledDisplayBorder = styled.div`
+  border-top: 1px solid rgba(0, 0, 0, 0.9);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.9);
+  border-left: 1px solid rgba(0, 0, 0, 0.7);
+  border-right: 1px solid rgba(0, 0, 0, 0.7);
+  border-radius: 4px;
+  box-shadow: 0 0 3px -1px rgba(0, 0, 0, 0.9);
+`;
 
 const StyledDisplay = styled.div`
   width: 100%;
   box-sizing: border-box;
   border: 3px solid red;
-  background-color: rgb(184, 189, 164);
+  background-color: rgb(154, 159, 134);
   display: flex;
   justify-content: flex-end;
   padding: 8px 24px;
 
-  border-top: 8px inset green;
-  border-left: 8px inset blue;
-  border-bottom: 16px inset red;
-  border-right: 8px inset yellow;
+  border-top: 8px inset rgba(0, 0, 0, 0.9);
+  border-left: 8px inset rgba(0, 0, 0, 0.8);
+  border-bottom: 16px inset rgba(0, 0, 0, 0.7);
+  border-right: 8px inset rgba(0, 0, 0, 0.8);
+
+  box-shadow: inset 0px 0px 5px -2px rgba(0, 0, 0, 0.9);
 `;
 
 const DisplayInner = styled.div`
   width: 100%;
-  background-color: yellow;
+  background-color: rgb(106, 117, 93);
+  display: flex;
+  justify-content: flex-end;
+  padding: 6px 12px 2px 12px;
+  border-radius: 8px;
+  box-shadow: inset 0px 3px 8px -2px rgba(0, 0, 0, 0.9);
+
+  transition: 0.1s ease-in;
+
+  &.power {
+    background-color: rgb(200, 214, 183);
+  }
 `;
 
 const DisplayBorder = (props) => {
-  return <StyledDisplay><DisplayInner>{props.children}</DisplayInner></StyledDisplay>;
+  const power = useStore((state) => state.power);
+
+  return (
+    <StyledDisplayBorder>
+      <StyledDisplay>
+        <DisplayInner className={power && "power"}>
+          {props.children}
+        </DisplayInner>
+      </StyledDisplay>
+    </StyledDisplayBorder>
+  );
 };
 
 export default DisplayBorder;
