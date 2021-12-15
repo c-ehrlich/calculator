fa4d4054d50d8b163562cc3ebad87af4a418a1ca
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-12-15 19:23:41 +0100

    give all buttons click functions

--- src/components/buttons/ButtonsLayout.jsx
+++ src/components/buttons/ButtonsLayout.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useMemo } from "react";
 import styled from "styled-components";
+import useStore from "../../store";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faDivide,
-  // faEquals,
+  // faEquals, // TODO bring back after passing FCC requirements
   faMinus,
   faPercent,
   faPlus,
@@ -20,6 +21,7 @@ const StyledButtonsLayout = styled.div`
   gap: 16px;
 `;
 
+// TODO bring back after passing FCC requirements
 // const WhiteDot = styled.div`
 //   width: 6px;
 //   height: 6px;
@@ -29,79 +31,104 @@ const StyledButtonsLayout = styled.div`
 // `;
 
 const ButtonsLayout = () => {
+  // TODO clear all passDownIDs after passing FCC requirements
+
+  const inputNumber = useStore((state) => state.inputNumber);
+  const inputPlus = useStore((state) => state.inputPlus);
+  const inputMinus = useStore((state) => state.inputMinus);
+  const inputTimes = useStore((state) => state.inputTimes);
+  const inputDivideBy = useStore((state) => state.inputDivideBy);
+  const inputInverse = useStore((state) => state.inputInverse);
+  const inputClear = useStore((state) => state.inputClear);
+  const inputEquals = useStore((state) => state.inputEquals);
+  const inputMPlus = useStore((state) => state.inputMPlus);
+  const inputMMinus = useStore((state) => state.inputMMinus);
+  const inputMRecall = useStore((state) => state.inputMRecall);
+  const inputMClear = useStore((state) => state.inputMClear);
+  const inputDecimal = useStore((state) => state.inputDecimal);
+  const inputNegative = useStore((state) => state.inputNegative);
+  const inputSqrt = useStore((state) => state.inputSqrt);
+  const inputPercent = useStore((state) => state.inputPercent);
+  
   return (
     <StyledButtonsLayout>
-      <Button type="green"><span>M +</span></Button>
-      <Button type="green">M –</Button>
-      <Button type="green">MR</Button>
-      <Button type="green" id="mc">
+      <Button type="green" clickFn={inputMPlus}>
+        <span>M +</span>
+      </Button>
+      <Button type="green" clickFn={inputMMinus}>
+        M –
+      </Button>
+      <Button type="green" clickFn={inputMRecall}>
+        MR
+      </Button>
+      <Button type="green" clickFn={inputMClear}>
         MC
       </Button>
-      <Button type="green" passDownId="negative">
+      <Button type="green" clickFn={inputNegative}>
         +/-
       </Button>
-      <Button type="brown" passDownId="sqrt">
+      <Button type="brown" clickFn={inputSqrt}>
         <FontAwesomeIcon icon={faSquareRootAlt} />
       </Button>
-      <Button type="black" passDownId="seven">
+      <Button type="black" passDownId="seven" clickFn={() => inputNumber(7)}>
         7
       </Button>
-      <Button type="black" passDownId="eight">
+      <Button type="black" passDownId="eight" clickFn={() => inputNumber(8)}>
         8
       </Button>
-      <Button type="black" passDownId="nine">
+      <Button type="black" passDownId="nine" clickFn={() => inputNumber(9)}>
         9
       </Button>
-      <Button type="brown" passDownId="divide">
+      <Button type="brown" passDownId="divide" clickFn={inputDivideBy}>
         <FontAwesomeIcon icon={faDivide} />
       </Button>
-      <Button type="brown" passDownId="percent">
+      <Button type="brown" passDownId="percent" clickFn={inputPercent}>
         <FontAwesomeIcon icon={faPercent} />
       </Button>
-      <Button type="black" passDownId="four">
+      <Button type="black" passDownId="four" clickFn={() => inputNumber(4)}>
         4
       </Button>
-      <Button type="black" passDownId="five">
+      <Button type="black" passDownId="five" clickFn={() => inputNumber(5)}>
         5
       </Button>
-      <Button type="black" passDownId="six">
+      <Button type="black" passDownId="six" clickFn={() => inputNumber(6)}>
         6
       </Button>
-      <Button type="brown" passDownId="multiply">
+      <Button type="brown" passDownId="multiply" clickFn={inputTimes}>
         <FontAwesomeIcon icon={faTimes} />
       </Button>
-      <Button type="brown" passDownId="clear">
-        <span>X<sup>-1</sup></span>
+      <Button type="brown" passDownId="inverse" clickFn={inputInverse}>
+        <span>
+          X<sup>-1</sup>
+        </span>
       </Button>
-      <Button type="black" passDownId="one">
+      <Button type="black" passDownId="one" clickFn={() => inputNumber(1)}>
         1
       </Button>
-      <Button type="black" passDownId="two">
+      <Button type="black" passDownId="two" clickFn={() => inputNumber(2)}>
         2
       </Button>
-      <Button type="black" passDownId="three">
+      <Button type="black" passDownId="three" clickFn={() => inputNumber(3)}>
         3
       </Button>
-      <Button type="brown" passDownId="subtract">
+      <Button type="brown" passDownId="subtract" clickFn={inputMinus}>
         <FontAwesomeIcon icon={faMinus} />
       </Button>
-      <Button type="brown" passDownId="clear-current">
+      <Button type="brown" passDownId="clear" clickFn={inputClear}>
         <span>CE</span>
         <span>C</span>
       </Button>
-      <Button type="black" passDownId="zero">
+      <Button type="black" passDownId="zero" clickFn={() => inputNumber(0)}>
         0
       </Button>
       {/* TODO put a circular div in here instead */}
-      <Button type="brown" passDownId="decimal">
-        .
-        {/* <WhiteDot /> */}
+      <Button type="brown" passDownId="decimal" clickFn={inputDecimal}>
+        .{/* <WhiteDot /> */}
       </Button>
-      <Button type="yellow" passDownId="equals">
-        {/* <FontAwesomeIcon icon={faEquals} /> */}
-        =
+      <Button type="yellow" passDownId="equals" clickFn={() => inputEquals()}>
+        {/* <FontAwesomeIcon icon={faEquals} /> */}=
       </Button>
-      <Button type="brown" passDownId="add" onClick={() => console.log("add")}>
+      <Button type="brown" passDownId="add" clickFn={inputPlus}>
         <FontAwesomeIcon icon={faPlus} />
       </Button>
     </StyledButtonsLayout>
