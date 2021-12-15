60b651a5f9d93286ddd0975b3562e02f0969ff33
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-12-15 19:24:17 +0100

    settings row styling

--- src/layout/SettingsRow.jsx
+++ src/layout/SettingsRow.jsx
@@ -6,6 +6,10 @@ const StyledSettingsRow = styled.div`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+
+  ${'' /* dirty hack: space reg/sci selector centred with button */}
+  ${'' /* better solution: actually center it, make the labels a :before or something */}
+  margin-right: 9px;
 `;
 
 const SettingsRow = (props) => {
