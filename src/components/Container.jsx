5b5e90ebcd02cf92f2bd4206ff0d2c0bf323930b
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-12-22 16:52:01 +0100

    background and calculator border/shadow

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,37 +1,71 @@
 import styled from "styled-components";
 
-import bg from "../media/CalcBG.png";
+import pageBG from "../media/bg-2400.jpg";
+import calcBG from "../media/CalcBG.png";
 
-const StyledContainer = styled.div`
+const Background = styled.div`
   height: 100%;
+  width: 100%;
   display: flex;
   color: red;
   align-items: center;
   justify-content: center;
+  background: url(${pageBG}) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
 `;
 
-const InnerContainer = styled.div`
+const CalculatorOuterContainer = styled.div`
+  border-left: 1.5px solid rgb(50, 50, 50);
+  border-top: 1.5px solid rgb(50, 50, 50);
+  border-right: 1px solid rgb(20, 20, 20);
+  border-bottom: 1px solid rgb(20, 20, 20);
+  background: rgb(246, 246, 246);
+  background: linear-gradient(
+    120.5deg,
+    rgba(246, 246, 246, 1) 46.5%,
+    rgba(62, 62, 62, 1) 50.5%,
+    rgba(11, 11, 11, 1) 54.5%
+  );
+  padding: 4px;
+  border-top-left-radius: 12px;
+  border-top-right-radius: 12px;
+  border-bottom-left-radius: 48px;
+  border-bottom-right-radius: 48px;
+  box-shadow: 
+    2px 2px 5px 2px rgba(0, 0, 0, 0.7),
+    4px 7px 5px 5px rgba(0, 0, 0, 0.5),
+    5px 10px 5px 10px rgba(0, 0, 0, 0.2),
+    inset 0 0 3px 3px rgba(0, 0, 0, 0.25);
+`;
+
+const CalculatorInnerContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 32px;
 
-  ${'' /* created with https://www.cssmatic.com/noise-texture */}
-  background-image: url(${bg});
-  background-repeat: repeat;
-
-  border: 6px outset rgba(0, 0, 0, 0.4);
   padding: 48px 32px;
-  border-top-left-radius: 16px;
-  border-top-right-radius: 16px;
-  border-bottom-left-radius: 48px;
-  border-bottom-right-radius: 48px;
+
+  border-top-left-radius: 6px;
+  border-top-right-radius: 6px;
+  border-bottom-left-radius: 44px;
+  border-bottom-right-radius: 44px;
+  box-shadow: 0 0 3px 3px rgba(0, 0, 0, 0.5);
+
+  ${"" /* created with https://www.cssmatic.com/noise-texture */}
+  background-image: url(${calcBG});
+  background-repeat: repeat;
 `;
 
 const Container = (props) => {
   return (
-    <StyledContainer>
-      <InnerContainer>{props.children}</InnerContainer>
-    </StyledContainer>
+    <Background>
+      <CalculatorOuterContainer>
+        <CalculatorInnerContainer>{props.children}</CalculatorInnerContainer>
+      </CalculatorOuterContainer>
+    </Background>
   );
 };
 
