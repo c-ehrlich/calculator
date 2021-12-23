9c09a65543af27a4ca501e1e227a9da774d9cf71
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-12-23 18:26:59 +0100

    create button active effects

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -14,21 +14,30 @@ const StyledButton = styled.button`
   font-family: "AGRegular";
   white-space: nowrap;
   border-radius: 50%;
-  box-shadow:
-    ${'' /* !!!! the yellow buttons have their own shadows */}
-    inset 17px 24px 40px -30px rgba(255, 255, 255, 0.4),
+  box-shadow: ${"" /* !!!! the yellow buttons have their own shadows */} inset
+      17px 24px 40px -30px rgba(255, 255, 255, 0.4),
     inset 6px 9px 5px -10px rgba(255, 255, 255, 0.3),
-    4px 7px 7px rgba(0, 0, 0, 0.3),
-    1.5px 3px 7px rgba(0, 0, 0, 0.6);
+    4px 7px 7px rgba(0, 0, 0, 0.3), 1.5px 3px 7px rgba(0, 0, 0, 0.6);
   cursor: pointer;
+
+  transition: all 0.1s ease-in-out;
+
+  &:hover {
+    box-shadow: inset 17px 24px 40px -30px rgba(255, 255, 255, 0.4),
+      inset 6px 9px 5px -10px rgba(255, 255, 255, 0.3),
+      0 0 15px -4px rgba(255, 255, 255, 0.7);
+  }
 
   &.black {
     background-color: black;
     color: white;
     font-size: 24px;
 
-    &:hover {
-      background-color: rgb(38, 38, 38);
+    &:active {
+      box-shadow: 0 0 15px -4px rgba(255, 255, 255, 0.7),
+        inset 7px 7px 20px rgba(0, 0, 0, 0.9),
+        inset -7px -7px 20px rgba(255, 255, 255, 0.4);
+      padding-top: 1.2px;
     }
   }
 
@@ -37,8 +46,11 @@ const StyledButton = styled.button`
     color: white;
     font-size: 17px;
 
-    &:hover {
-      background-color: rgb(103, 39, 4);
+    &:active {
+      box-shadow: 0 0 15px -4px rgba(255, 255, 255, 0.7),
+        inset 7px 7px 20px rgba(26, 9, 1, 0.9),
+        inset -7px -7px 20px rgba(255, 255, 255, 0.4);
+      padding-top: 3px;
     }
   }
 
@@ -46,9 +58,12 @@ const StyledButton = styled.button`
     background-color: rgb(0, 46, 19);
     color: white;
     font-size: 20px;
-    
-    &:hover {
-      background-color: rgb(0, 97, 40);
+
+    &:active {
+      box-shadow: 0 0 15px -4px rgba(255, 255, 255, 0.7),
+        inset 7px 7px 20px rgba(0, 28, 12, 0.9),
+        inset -7px -7px 20px rgba(4, 145, 65, 0.5);
+      padding-top: 2.5px;
     }
   }
 
@@ -56,14 +71,22 @@ const StyledButton = styled.button`
     background-color: rgb(217, 145, 0);
     color: black;
     font-size: 18px;
-    box-shadow:
-      inset 17px 24px 40px -30px rgba(255, 255, 255, 0.9),
+    box-shadow: inset 17px 24px 40px -30px rgba(255, 255, 255, 0.9),
       inset 6px 9px 5px -10px rgba(255, 255, 255, 0.5),
-      4px 7px 7px rgba(0, 0, 0, 0.2),
-      1.5px 3px 7px rgba(0, 0, 0, 0.5);
-    
+      4px 7px 7px rgba(0, 0, 0, 0.2), 1.5px 3px 7px rgba(0, 0, 0, 0.5);
+
     &:hover {
-      background-color: rgb(255, 184, 41);
+      box-shadow: 0 0 15px -4px rgba(255, 255, 255, 0.7),
+        inset 17px 24px 40px -30px rgba(255, 255, 255, 0.9),
+        inset 6px 9px 5px -10px rgba(255, 255, 255, 0.5);
+        
+    }
+
+    &:active {
+      box-shadow: 0 0 15px -4px rgba(255, 255, 255, 0.7),
+        inset 7px 7px 20px rgba(92, 60, 1, 0.8),
+        inset -7px -7px 25px rgba(255, 255, 255, 0.5);
+      padding-top: 3px;
     }
   }
 `;
