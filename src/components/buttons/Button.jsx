0418c47c0106330e80d52caa0ee98facdd0ecedc
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-12-23 16:27:57 +0100

    create button shadows and inset shadows

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -15,38 +15,48 @@ const StyledButton = styled.button`
   white-space: nowrap;
   border-radius: 50%;
   box-shadow:
-    0px 3px 8px #111,
-    inset 0px 2px 3px #777;
+    ${'' /* !!!! the yellow buttons have their own shadows */}
+    inset 17px 24px 40px -30px rgba(255, 255, 255, 0.4),
+    inset 6px 9px 5px -10px rgba(255, 255, 255, 0.3),
+    4px 7px 7px rgba(0, 0, 0, 0.3),
+    1.5px 3px 7px rgba(0, 0, 0, 0.6);
   cursor: pointer;
-  ${(props) => {
-    switch (props.type) {
-      case "black":
-        return `background-color: black;
-                color: white;
-                font-size: 24px;`;
-      case "brown":
-        return `background-color: rgb(88, 65, 15);
-                color: white;
-                font-size: 17px;`;
-      case "green":
-        return `background-color: rgb(1, 75, 32);
-                color: white;
-                font-size: 20px;`;
-      case "yellow":
-        return `background-color: rgb(253, 219, 28);
-                color: black;
-                font-size: 18px;`;
-      default:
-        return `background-color: pink;`;
-    }
-  }}
+
+  &.black {
+    background-color: black;
+    color: white;
+    font-size: 24px;
+  }
+
+  &.brown {
+    background-color: rgb(54, 20, 2);
+    color: white;
+    font-size: 17px;
+  }
+
+  &.green {
+    background-color: rgb(0, 46, 19);
+    color: white;
+    font-size: 20px;
+  }
+
+  &.yellow {
+    background-color: rgb(217, 145, 0);
+    color: black;
+    font-size: 18px;
+    box-shadow:
+      inset 17px 24px 40px -30px rgba(255, 255, 255, 0.9),
+      inset 6px 9px 5px -10px rgba(255, 255, 255, 0.5),
+      4px 7px 7px rgba(0, 0, 0, 0.2),
+      1.5px 3px 7px rgba(0, 0, 0, 0.5);
+  }
 `;
 
 const Button = (props) => {
   return (
     <StyledButton
       id={props.passDownId}
-      type={props.type}
+      className={`${props.type}`}
       onClick={props.clickFn && props.clickFn}
     >
       {props.children}
